0ebaf1b4c4b63c6e60dd5280c26d45905cf260d7
youdy <benyoud@outlook.com>
2023-01-25 20:48:07 +0000

    Context Module Functions - complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -73,14 +73,21 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  console.log(user)
+  userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
 
-// export {UserProvider, useUser}
+
+
+// export {UserProvider, useUser, updateUser}
 
 // src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
+// import {UserProvider, useUser, } from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -95,16 +102,12 @@ function UserSettings() {
     setFormState({...formState, [e.target.name]: e.target.value})
   }
 
+  
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState)
   }
-
+  
   return (
     <form onSubmit={handleSubmit}>
       <div style={{marginBottom: 12}}>
